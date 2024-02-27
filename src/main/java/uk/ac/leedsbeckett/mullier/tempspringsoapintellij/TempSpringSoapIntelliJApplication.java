18693c505454029137223eaa3e578f416357a3cc
Duncan Mullier <D.Mullier@leedsbeckett.ac.uk>
2024-02-27 13:52:55 +0000

    Initial commit

---
+++ src/main/java/uk/ac/leedsbeckett/mullier/tempspringsoapintellij/TempSpringSoapIntelliJApplication.java
@@ -0,0 +1,13 @@
+package uk.ac.leedsbeckett.mullier.tempspringsoapintellij;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TempSpringSoapIntelliJApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TempSpringSoapIntelliJApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uk/ac/leedsbeckett/mullier/tempspringsoapintellij/TempSpringSoapIntelliJApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.ac.leedsbeckett.mullier.tempspringsoapintellij;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TempSpringSoapIntelliJApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
